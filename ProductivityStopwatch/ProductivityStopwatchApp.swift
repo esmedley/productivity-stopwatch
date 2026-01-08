398b03953d29950ae8ae977f7a4fb72fdcda5c44
Erin Smedley <erinsmedley3@gmail.com>
2026-01-08 22:12:35 +0000

    Initial Commit

---
+++ ProductivityStopwatch/ProductivityStopwatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductivityStopwatchApp.swift
+//  ProductivityStopwatch
+//
+//  Created by esmedley on 08/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct ProductivityStopwatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
